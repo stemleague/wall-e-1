798a395723b04a3072c510e40e8cabc187c8ed9f
stem league <64894084+stemleague@users.noreply.github.com>
2021-08-09 16:21:47 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+  # prints out intro to our program!
+  print("Hi I am WALL- E (Waste Allocation Load Lifter - Earth-Class). I am a waste-collector drone programmed to haul the garbage plaguing Earth in the distant future.")
+  print()
+
+  choice = "check charge"
+
+  if choice == "check charge":
+    # Prints name of program
+    print("Welcome to the Charge Level Program.")
+    # Provide battery percentage of WALL-E (0-100)
+    percentage = 90
+
+    # TODO: Write the conditionals to check the percentage and print out a message:
+    #   - if the battery percentage is less than 10, then print "BEEP BEEP! You are low in battery."
+    #   - if the battery percentage is more than 90, print "You are fully charged."
+    #   - if battery percentage is anything in between 10-90 then print "Battery Level Moderate."
